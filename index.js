5760181b37efc22c7b907a7414ec7e787778c415
James Hawk <jph94880@gmail.com>
2023-04-12 21:15:16 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, name) {
+        return drivers.filter(function(driverName) {
+            return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch (drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.startsWith(name);
+    })
+    
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.name === name;
+    }
+    )
+}
